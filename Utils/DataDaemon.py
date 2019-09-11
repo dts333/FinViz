59acdd901b0503fd37ed99243fecf61e5786cb7e
Danny Swift <dswift333@gmail.com>
2019-09-11 16:42:00 -0400

    scheduler

--- Utils/DataDaemon.py
+++ Utils/DataDaemon.py
@@ -1,4 +1,5 @@
-import Configs.config as config
+from apscheduler.schedulers.blocking import BlockingScheduler
+from Configs import config
 import requests
 import pandas as pd
 import io
@@ -7,6 +8,7 @@ import io
 class DataDaemon:
     def __init__(self):
         self.URL = "https://www.alphavantage.co/query?"
+        self.last_updated = config.LAST_UPDATED
 
         self.headers = {
             'apikey': config.ALPHAVANTAGE_APIKEY,
@@ -15,19 +17,29 @@ class DataDaemon:
             'datatype': 'csv'
         }
 
-    #AlphaVantage is limiting me to 500 API calls/day - not really what I want
-    def gather_alphavantage_data(self):
+    def gather_alphavantage_data(self, ticker):
         headers = self.headers
-        path = config.PATH
-        tickers = pd.read_csv(path + "Data\\EquityLists\\NYSEcompanylist.csv")
-        tickers = tickers.merge(pd.read_csv(path + "Data\\EquityLists\\NASDAQcompanylist.csv"))
+        headers['symbol'] = ticker
+        data = requests.get(self.URL, self.headers)
+        data.raw.decode_content = True
+        #with open(path + 'Data\\AlphaVantage\\' + ticker + '.csv', 'wb') as file:
+        #    for chunk in data:
+        #        file.write(chunk)
+        try:
+            df = pd.read_csv(path + 'Data\\AlphaVantage\\' + ticker + '.csv')
+        except FileNotFoundError:
+            pass
+        
 
-        for _, row in tickers.iterrows():
-            ticker = row["Symbol"]
-            headers['symbol'] = ticker
-            data = requests.get(self.URL, self.headers)
-            data.raw.decode_content = True
-            with open(path + 'Data\\AlphaVantage\\' + ticker + '.csv', 'wb') as file:
-                for chunk in data:
-                    file.write(chunk)
 
+@sched.scheduled_job('cron', hour=4)
+def update():
+    daemon = DataDaemon()
+    path = config.PATH
+    tickers = pd.read_csv(path + "Data\\EquityLists\\NYSEcompanylist.csv")
+    tickers = tickers.merge(pd.read_csv(path + "Data\\EquityLists\\NASDAQcompanylist.csv"))
+
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler(timezone='EST')
+    scheduler.start()
