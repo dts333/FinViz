cfb38541b0d3ef15964d9b1e064940e97387547e
Danny Swift <dswift333@gmail.com>
2019-09-12 16:23:55 -0400

    daemon can now gather AV data

--- Utils/DataDaemon.py
+++ Utils/DataDaemon.py
@@ -1,44 +1,70 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from Configs import config
+from configparser import ConfigParser
+import config
+import os
 import requests
 import pandas as pd
+import time
 import io
 
 
 class DataDaemon:
     def __init__(self):
         self.URL = "https://www.alphavantage.co/query?"
-        self.last_updated = config.LAST_UPDATED
+        self.path = config.PATH
 
         self.headers = {
-            'apikey': config.ALPHAVANTAGE_APIKEY,
+            'apikey': config.APIKEY,
             'function': 'TIME_SERIES_DAILY',
             'outputsize': 'full',
             'datatype': 'csv'
         }
 
-    def gather_alphavantage_data(self, ticker):
+    def get_alphavantage_data(self, ticker):
         headers = self.headers
         headers['symbol'] = ticker
+        filename = self.path + 'Data' + os.sep + 'AlphaVantage' + os.sep + ticker + '.csv'
         data = requests.get(self.URL, self.headers)
         data.raw.decode_content = True
-        #with open(path + 'Data\\AlphaVantage\\' + ticker + '.csv', 'wb') as file:
-        #    for chunk in data:
-        #        file.write(chunk)
         try:
-            df = pd.read_csv(path + 'Data\\AlphaVantage\\' + ticker + '.csv')
+            df = pd.read_csv(filename)
         except FileNotFoundError:
             pass
+        if data.status_code == 200:
+            data = pd.read_csv(io.StringIO(data.text))
+        try:
+            if 'data' in locals() and 'df' in locals():
+                data.merge(df, how='outer', on='timestamp')
+                del df
+        except:
+            print(df.head()) 
+            print(data.head())
         
+        data.to_csv(filename)
+        del data
 
 
 @sched.scheduled_job('cron', hour=4)
 def update():
     daemon = DataDaemon()
     path = config.PATH
-    tickers = pd.read_csv(path + "Data\\EquityLists\\NYSEcompanylist.csv")
-    tickers = tickers.merge(pd.read_csv(path + "Data\\EquityLists\\NASDAQcompanylist.csv"))
-
+    last_updated = config.LAST_UPDATED
+    tickers = pd.read_csv(path + 'Data' + os.sep + 'EquityLists' 
+                          + os.sep + 'NYSEcompanylist.csv')
+    tickers = tickers.merge(pd.read_csv(path + 'Data' + os.sep + 'EquityLists' 
+                                        + os.sep + 'NASDAQcompanylist.csv'), how = 'outer')
+    if last_updated == '':
+        index = 0
+    else:
+        index = tickers[tickers['Symbol'] == last_updated].index[0]
+    tickers = tickers.iloc[index:index+500]
+    i=0
+    for _, row in tickers.iterrows():
+        daemon.get_alphavantage_data(row['Symbol'])
+        time.sleep(12)
+        i += 1
+        print(str(i) + ' ' + row['Symbol'])
+    
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler(timezone='EST')
